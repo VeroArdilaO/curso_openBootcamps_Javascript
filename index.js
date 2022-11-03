efad58ffc09126472e652e0564e4ec0260071bec
Veronica <veronica.ardilao@outlook.es>
2022-11-02 22:18:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Este es el mensaje de Bievenida
+
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*Esta es la parte intermedia del proyecto
+Test
+Test, esto es un comentario*/
+
+//Este va a ser el mensaje para despedir
+
+console.log("Adios")
