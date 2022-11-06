681a443a86a10e49f8798e983b67437c17d5e1df
Veronica <veronica.ardilao@outlook.es>
2022-11-06 16:31:58 -0500

    Ejercicio 2 variables JS

---
+++ mi-primer-proyecto-node/variables.js
@@ -0,0 +1,17 @@
+const Nombre = "Veronica";
+
+const Edad = 27;
+
+let developer = true;
+
+let nacimiento = new Date("5/13/95");
+
+let libroFavorito = {
+    titulo: "El caballero de la armadura oxidada",
+    autor: "Robert Fisher",
+    fecha: 1/5/87,
+    url: "https://www.panamericana.com.co/el-caballero-de-la-armadura-oxidada/p?idsku=235912&utm_source=google&utm_medium=cpa&utm_campaign=Co_Panamericana_google_performance_categoria_libros_aon_categoria_libros_shopping_standar&utm_term=shopping_standar&gclid=CjwKCAjwtp2bBhAGEiwAOZZTuApCmeorGh0V_S0UG5uZxI_rrSqQgCxUcyojM4jz94Jh6ermKx0UfxoCQk4QAvD_BwE",
+}
+
+
+
