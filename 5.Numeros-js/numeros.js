be72901301ef05a33b769feb90ef81377855563c
Veronica <veronica.ardilao@outlook.es>
2022-11-13 17:49:32 -0500

    Numeros en Javascript

---
+++ 5.Numeros-js/numeros.js
@@ -0,0 +1,13 @@
+let alturaCentimetros = 154;
+
+let alturaMetro = 1.54;
+
+let pesoKilogramos = 40.5;
+
+let alturaRedondeadaArriba = Math.ceil(alturaMetro);
+
+let pesoRedondeadoAbajo = Math.floor(pesoKilogramos);
+
+let message = "el máximo valor que se puede obtener en Javascript + 1";
+ 
+console.log(Math.max(message));
